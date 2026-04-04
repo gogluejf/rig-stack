837d758f0bf7a6bd63ae3040bc124c01f9ff7dd6
jf rene <renejf@gmail.com>
2026-04-04 20:26:58 +0000

    fix idempotent for ndvdia keyring

--- scripts/setup/03-install-nvidia-toolkit.sh
+++ scripts/setup/03-install-nvidia-toolkit.sh
@@ -38,7 +38,7 @@ echo "Installing NVIDIA Container Toolkit..."
 
 # Add NVIDIA Container Toolkit repository
 curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | \
-    sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg
+    sudo gpg --batch --yes --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg
 
 curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | \
     sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
