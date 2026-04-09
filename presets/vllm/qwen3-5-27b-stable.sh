e4e410d17cb346a84f6b54490837e55a8fe8af90
jf rene <renejf@gmail.com>
2026-04-09 20:10:51 +0000

    fix qwen stable 60t/s

--- presets/vllm/qwen3-5-27b-stable.sh
+++ presets/vllm/qwen3-5-27b-stable.sh
@@ -9,14 +9,14 @@ VLLM_ARGS=(
   --override-generation-config '{"temperature": 0.8, "top_k": -1, "min_p": 0.05, "frequency_penalty": 0.3, "top_p": 0.95}'
   --served-model-name Kbenkhaled/Qwen3.5-27B-NVFP4
   --max-model-len 65536
+  --max-num-seqs 1 \
   --kv-cache-dtype fp8
   --enable-prefix-caching
   --tool-call-parser qwen3_coder
-  --gpu-memory-utilization 0.85
+  --gpu-memory-utilization 0.82
   --tensor-parallel-size 1
   --dtype auto
   --trust-remote-code
-  --enforce-eager
   --host 0.0.0.0
   --port 8000
 )
